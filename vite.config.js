978fe173cdfa4033f5dd8e79278f79d80a1d3aef
mainaerick <niteric@gmail.com>
2025-10-21 05:37:54 +0300

    redo docker change

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.tsx',
-            base: process.env.ASSET_URL,
-            refresh: true,
-        }),
-        react(),
-    ],
+    return {
+        base: env.ASSET_URL
+            ? `${env.ASSET_URL}/build/`
+            : '/build/',
+
+        plugins: [
+            laravel({
+                input: ['resources/js/app.tsx'],
+                ssr: 'resources/js/ssr.tsx',
+                refresh: true,
+            }),
+            react(),
+        ],
+    };
 });
